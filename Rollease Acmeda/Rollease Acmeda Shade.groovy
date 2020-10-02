10d876560afbafcf752884323f2be39d97be9eb9
Kick4U2 <39640638+Kick4U2@users.noreply.github.com>
2020-10-02 09:16:54 -0400

    Update Rollease Acmeda Shade.groovy
    This can be used with the Lutron Integration App and Button Controller App to simulate the Rollease Shades as a Lutron Shade with Mirror Me.  With this driver, the Lutron system operates the Rollease Shade in the similar way it would a Lutron Shade.

--- Rollease Acmeda/Rollease Acmeda Shade.groovy
+++ Rollease Acmeda/Rollease Acmeda Shade.groovy
@@ -1,5 +1,9 @@
-/* 
+/*
+Version 2020.10.02
+Release Notes:
+- Optimized to be used with Lutron Integration and Mirror Me
 
+Revision History
 Version 2020.08.16
 Release Notes:
 - Reduced whitespace
@@ -7,33 +11,23 @@ Release Notes:
 - Removed moving attribute (windowShade comes with opening and closing attributes)
 - Cleaned voltage attribute
 - Cleaned parse function
-
-
-Revision History
-
 Version 2020.06.08.01
 Release Notes:
 - Added "closing", "opening" and "partially open" states to the windowShade attribute
 - Added "toggle" command
-
 Version 2020.03.30 
 - Initial Release
-
 */
 
 metadata {
 	definition (name: "Rollease Acmeda Shade", namespace: "arcautomate", author: "Younes Oughla", vid: "generic-shade") {
-		capability "Actuator"
-		capability "Initialize"
 		capability "Refresh"
 		capability "Switch"
-		capability "VoltageMeasurement"
-		capability "WindowShade"
-		command  "stop"
-		command  "toggle"
+		capability "SwitchLevel"
+		command "stop"
+		command "toggle"
 		attribute "lastDirection", "enum"
-		attribute "position", "int"
-		attribute "voltage","int"
+		attribute "moving", "enum"
 	}
 	preferences {
 		input ("motorAddress", "STRING", title: "Motor Address", description: "", defaultValue: "000", required: true, displayDuringSetup: true )
@@ -68,93 +62,66 @@ def stop() {
 
 def toggle() {
 	logDebug "Toggling Shade"
-	currentStatus = device.currentValue("windowShade")
-	switch (currentStatus) {
-		case "opening":
-		case "closing":
-			stop()
-			break;
-		case "open":
-			close()
-			break;
-		case "closed":
-			open()
-			break;
-		case "partially open":
-        		if (device.currentValue("lastDirection") == "true") {
-				close()
-			} else {
-				open()
-			}
-			break;
-		default:
-			open()
+    if (device.currentValue("moving") == "true") {
+		stop()
+    } else if (device.currentValue("lastDirection") == "true") {
+		close()
+	} else {
+		open()
 	}
 }
 
-def setPosition(position) {
-	logDebug "Set Position: ${position}"
-	positionString = 100-position
-	positionString = "000${positionString}"
-	positionString = positionString[-3..-1]
-	sendCommand("m",positionString)
+def setLevel(level) {
+	logDebug "Set Level: ${level}"
+	levelString = 100-level
+	levelString = "000${levelString}"
+	levelString = levelString[-3..-1]
+	sendCommand("m", levelString)
 }
 
 def sendCommand(String command, String commandData) {
 	commandString = "!${motorAddress}${command}${commandData}"
-	logDebug "Send Command: ${commandString}"
+	logDebug "Command Tx: ${commandString}"
 	parent.sendTelnetCommand(commandString)
 }
 
 def parse(String msg) {
+    logDebug "Command Rx: ${msg}"
 	switch (msg.substring(4, 5)) {
-		case "r":
-			positionStr = msg.substring(5, 8)
-			position = 100 - Integer.parseInt(positionStr)
-			positionUpdated(position)
-			break;
+   	    case "r":
+			sendEvent(name: "moving", value: false)
+			levelStr = msg.substring(5, 8)
+			level = 100 - Integer.parseInt(levelStr)
+	        if (level == 100) {
+				sendEvent(name: "switch", value: true)
+			} else if (level == 0) {
+				sendEvent(name: "switch", value: false)
+			} else {
+				sendEvent(name: "switch", value: true)
+			}
+			sendEvent(name: "level", value: level)
+		break;
 		case "m":
-			positionStr = msg.substring(5, 8)
-			targetPosition = 100 - Integer.parseInt(positionStr)
-			currentPosition = device.currentValue("position")
-			if (targetPosition > currentPosition) {
-				sendEvent(name: "windowShade", value: "opening")
-                sendEvent(name: "lastDirection", value: true)
+			sendEvent(name: "moving", value: true)
+			levelStr = msg.substring(5, 8)
+			level = 100 - Integer.parseInt(levelStr)
+			currentLevel = device.currentValue("level")
+			if (level > currentLevel) {
+				sendEvent(name: "switch", value: true)
+				sendEvent(name: "lastDirection", value: true)
+			} else if (level < currentLevel) {
+				sendEvent(name: "lastDirection", value: false)
 			}
-			if (targetPosition < currentPosition) {
-				sendEvent(name: "windowShade", value: "closing")
-                sendEvent(name: "lastDirection", value: false)
-			}
-			break;
-		case "p":
-			if (msg.substring(5, 7) == "Vc") {
-				voltageStr = msg.substring(7, 12)
-				voltageStr = Integer.parseInt(voltageStr) / 100
-				sendEvent(name: "voltage", value: voltageStr)
-			}
-			break;
+		break;
+		case "s":
+			sendEvent(name: "moving", value: false)
+		break;
 	}
-}
-
-def positionUpdated(position) {
-	logDebug "Position Updated: ${position}"
-	if (position == 100) {
-		sendEvent(name: "windowShade", value: "open")
-	} else if (position == 0) {
-		sendEvent(name: "windowShade", value: "closed")
-	} else {
-		sendEvent(name: "windowShade", value: "partially open")
-	}
-	sendEvent(name: "position", value: position)
-}
-
-def requestStatus() {
-	sendCommand("r","?")
 }
 
 def refresh() {
 	logDebug "Refreshing"
-	requestStatus()
+	sendCommand("r","?")
 }
 
 private def logDebug(message) {
